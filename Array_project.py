64e9749244ce9c5fd7eda397e46de19689319128
samarbaike <samarb16k@gmail.com>
2024-11-24 12:50:44 +0600

    defining functions for relocation

--- Array_project.py
+++ Array_project.py
@@ -2,6 +2,19 @@ from random import randrange
 distance = [randrange (1, 7) for i in range(3)]
 weight = [randrange (1, 300) for i in range(3)]
 
-while weight != 713:
-    weight = [randrange (1, 300) for i in range(3)]
+
+previous = set(distance)
+
+def relocate(current, used):
+    
+    new_locations = []
+    for l in current:
+        while True:
+            new = randrange(1, 7)
+            if new != l and new not in used:
+                new_locations.append(new)
+                used.add(new)  # Add to the set of used locations
+                break
+    return new_locations
+
 
---
+++ trial#2.py
@@ -0,0 +1,2 @@
+while sum(weight) != 713:
+    weight = [randrange (1, 300) for i in range(3)]
