d4646e9a4051788e23d2656afaa15c8828ab5f3b
samarbaike <samarb16k@gmail.com>
2024-11-26 13:42:37 +0600

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,7 +1,19 @@
-a =[int(s) for s in input().split()]
-for i in range(0, len(a)-4, 2):
-    if i ==0:
-        print('YES')
-        break
-    else:
-        print('Not')
+a =[]
+a11=[int(a1) for a1 in input().split()]
+a.extend(a11)
+a12=[int(a2) for a2 in input().split()]
+a.extend(a12)
+a13=[int(a3) for a3 in input().split()]
+a.extend(a13)
+a14=[int(a4) for a4 in input().split()]
+a.extend(a14)
+a15=[int(a5) for a5 in input().split()]
+a.extend(a15)
+a16=[int(a6) for a6 in input().split()]
+a.extend(a16)
+a17=[int(a7) for a7 in input().split()]
+a.extend(a17)
+a18=[int(a8) for a8 in input().split()]
+a.extend(a18)
+
+print(a)
