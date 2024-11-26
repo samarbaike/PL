18f189d1f95506ebb45d0e0cbb87fee87e111479
samarbaike <samarb16k@gmail.com>
2024-11-26 10:28:00 +0600

    just some trial codes

---
+++ trial#2.py
@@ -0,0 +1,5 @@
+a =[]
+a11=[int(a1) for a1 in input().split()]
+a.extend(a11)
+
+print(a)
---
+++ trial.py
@@ -0,0 +1,7 @@
+a =[int(s) for s in input().split()]
+for i in range(0, len(a)-4, 2):
+    if i ==0:
+        print('YES')
+        break
+    else:
+        print('Not')
