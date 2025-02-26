27915f10e44a5bcc7fb2084e568fd138f79e9e49
samarbaike <samarb16k@gmail.com>
2025-02-26 11:00:29 +0600

    trying something

--- trial#2.py
+++ trial#2.py
@@ -1,3 +1,4 @@
-a = list(input())
-if a == list(reversed(a)):print(True)
-else:print(False)
+import pandas as sm
+fact = sm.read_csv('factbook.csv',header=0, sep=';', skiprows=[1]).head(10)
+fact['Country']=fact['Country'].map({j: i for i, j in enumerate(fact["Country"])})
+print(fact)
